4a5686e6ca6cf14dfaa37deb86e7f9c86076bb00
matsui614 <k-matsui@tsuki-nowa.com>
2025-02-27 16:01:04 +0900

    Initial Commit

---
+++ SlideshowApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SlideshowApp
+//
+//  Created by 松居昴希 on 2025/02/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SlideshowAppTests/SlideshowAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  SlideshowAppTests.swift
+//  SlideshowAppTests
+//
+//  Created by 松居昴希 on 2025/02/27.
+//
+
+import Testing
+@testable import SlideshowApp
+
+struct SlideshowAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
